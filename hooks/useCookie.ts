b060396759b9bd7b53e80e45c5c0fc583b7fda47
Alex <axlisovsky@gmail.com>
2023-01-29 05:06:49 +0200

    domain

--- hooks/useCookie.ts
+++ hooks/useCookie.ts
@@ -20,8 +20,8 @@ export function getCookie(key: string, options?: OptionsType | undefined) {
 export function setCookie(key: string, value: any) {
   setNextCookie(key, JSON.stringify(value), {
     maxAge: 60 * 60 * 24,
-    sameSite: false,
-    domain: 'https://energywave-client.vercel.app',
+    sameSite: true,
+    // domain: 'https://energywave-client.vercel.app',
   }) //1 month
 }
 
